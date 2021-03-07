1edce83afb3e77e5a7466c724e55e13d1e353c71
KKM-dev <cirill@mail.ru>
2021-03-08 02:40:29 +0300

    hw_lesson_4

--- mainapp/views.py
+++ mainapp/views.py
@@ -10,28 +10,10 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-def products(request):
-    context = {'title': 'GeekShop - Каталог',
-               'products_menu': ['Новинки', 'Одежда', 'Обувь', 'Аксессуары', 'Подарки'],
-               'products': Product.objects.all(),
-               'categories': ProductCategory.objects.all(),
-               }
-    return render(request, 'mainapp/products.html', context)
-
-
-def test_context(request):
+def products(request, id=None):
     context = {
-        'title': 'geekshop',
-        'header': 'Добро пожаловать на сайт!',
-        'username': 'Иван Иванов',
-        'products': [
-            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': '6 090,00'},
-            {'name': 'Синяя куртка The North Face', 'price': '23 725,00'},
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': '3 390,00'},
-        ],
-        'products_of_promotion': [
-            {'name': 'Черный рюкзак Nike Heritage', 'price': '2 340,00'},
-            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': '13 590,00'},
-        ]
+        'title': 'GeekShop - Каталог',
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
     }
-    return render(request, 'mainapp/test-context.html', context)
+    return render(request, 'mainapp/products.html', context)
