47f534caf841d0ab88394d35cee6fc25ae7c1d2b
KKM-dev <cirill@mail.ru>
2021-03-22 01:03:41 +0300

    hw lesson 8

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.core.paginator import Paginator
 from mainapp.models import ProductCategory, Product
 
 
@@ -10,10 +10,13 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-def products(request, id=None):
-    context = {
-        'title': 'GeekShop - Каталог',
-        'products': Product.objects.all(),
-        'categories': ProductCategory.objects.all(),
-    }
+def products(request, category_id=None, page=1):
+    context = {'title': 'GeekShop - Каталог', 'categories': ProductCategory.objects.all()}
+    if category_id:
+        products = Product.objects.filter(category_id=category_id).order_by('-price')
+    else:
+        products = Product.objects.all().order_by('-price')
+    paginator = Paginator(products, 3)
+    products_paginator = paginator.page(page)
+    context.update({'products': products_paginator})
     return render(request, 'mainapp/products.html', context)
